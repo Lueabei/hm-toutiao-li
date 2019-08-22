410aaf6020f826cb120cca0e0e4428c6d00632fd
lue <845741207@qq.com>
2019-08-22 21:46:26 +0800

    第一天-登录页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,6 +3,21 @@
         <!-- Login -->
         <el-card class="my-card">
             <img src="../../assets/images/logo_index.png" alt="">
+            <el-form :model="loginForm">
+                <el-form-item>
+                    <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:236px;margin-right:10px"></el-input>
+                    <el-button>发送验证码</el-button>
+                </el-form-item>
+                 <el-form-item>
+                    <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" style="width:100%">登录</el-button>
+                </el-form-item>
+            </el-form>
         </el-card>
 
     </div>
@@ -10,6 +25,14 @@
 
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
